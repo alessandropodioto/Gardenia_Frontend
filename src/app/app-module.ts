b9b2109576fc8646103e92a89fec44caa797ac12
marcocretu0 <marcocretu0@gmail.com>
2026-03-25 11:01:51 +0100

    1

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,21 +1,35 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 
+import { CarrelloComponent } from './carrello/carrello';
+
 @NgModule({
-  declarations: [
-    App
-  ],
+
+  declarations: [App, CarrelloComponent], 
   imports: [
     BrowserModule,
-    AppRoutingModule
+   
+    BrowserAnimationsModule,
+    HttpClientModule,
+    
+    AppRoutingModule,
+
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressBarModule,
   ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideClientHydration(withEventReplay()),
-  ],
-  bootstrap: [App]
+  providers: [],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
