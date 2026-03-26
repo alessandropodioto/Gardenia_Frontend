b9bc5d998fb049283688aa8adabbafb5e044c85a
marcocretu0 <marcocretu0@gmail.com>
2026-03-26 11:04:48 +0100

    pagamento aggiunto

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -4,13 +4,17 @@ import { ProductDetails } from './components/product-details/product-details';
 import { HomeComponent } from './components/home/home';
 import { Login } from './components/login/login';
 import { Register } from './components/register/register';
+import { CarrelloComponent } from './components/carrello/carrello';
+import { PagamentoComponent } from './components/pagamento/pagamento';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
   { path: 'product' , component:ProductDetails },
-  { path: 'home', component: HomeComponent }
+  { path: 'cart', component: CarrelloComponent },
+  { path: 'home', component: HomeComponent },
+ { path: 'pagamento', component: PagamentoComponent }
 ];
 
 @NgModule({
